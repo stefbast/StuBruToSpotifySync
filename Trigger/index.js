5978484a580aa9b1ff4e43b24924bc3ad16899c0
Stefaan Bastin <stefaan.bastin@gmail.com>
2016-07-14 21:55:34 +0200

    Create spotify api object in trigger

--- Trigger/index.js
+++ Trigger/index.js
@@ -1,10 +1,19 @@
 var sync = require('../sync.js');
+var SpotifyWebApi = require('spotify-web-api-node');
 
 module.exports = function (context, myTimer) {
     
     var timeStamp = new Date().toISOString();
 
     context.log('Start syncing !', timeStamp);
+
+    var user = 'TODO'
+
+    var spotifyApi = new SpotifyWebApi({
+    clientId : 'TODO',
+    clientSecret: 'TODO',
+    redirectUri : 'http://www.example.com/callback'
+    });
 
     sync.importList(spotifyApi, 'TODO', user, 'De afrekening');
 
