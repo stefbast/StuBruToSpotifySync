f8291c76a61141e247add8028bc4ccdf3f29689b
Stefaan Bastin <stefaan.bastin@gmail.com>
2016-09-29 11:19:42 +0200

    logging also to file

--- Trigger/index.js
+++ Trigger/index.js
@@ -1,12 +1,20 @@
 module.exports = function (context, myTimer) {
 
+    var logfile = "D:/home/site/wwwroot/log.txt";
+    var newLine = "\r\n"    
+    var fs = require('fs');
+    fs.writeFileSync(logfile, "Sync from:" + new Date().toISOString() + newLine);
+
     console.log = function(message, error){
         if(error){
             context.log(message, error);
+            fs.appendFileSync(logfile, message + error + newLine)
         }else{
             context.log(message);
+            fs.appendFileSync(logfile, message + newLine)
         }            
     };
+
 
     var sync = require('../sync.js');
     var SpotifyWebApi = require('spotify-web-api-node');
